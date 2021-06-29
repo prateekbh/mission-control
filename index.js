6e324878b882e6cec07cf92e75ad106dade57e61
Prateek Bhatnagar <prateek89born@gmail.com>
2021-06-29 12:14:57 +0530

    hygiene

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const { app, BrowserWindow } = require("electron");
+const path = require('path');
+
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    }
   });
-
+  win.webContents.openDevTools()
   win.loadFile("index.html");
+
+  setTimeout(() => {
+    win.webContents.send('asynchronous-message', {'SAVED': 'File Saved'});
+  }, 5000)
 }
 
 app.whenReady().then(() => {
